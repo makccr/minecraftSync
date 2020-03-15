6bc057c34b0c558a9de7ccc531701164a2343cf0
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2020-03-15 02:59:40 -0500

    Getting all the guts of the script working

--- osx/setup.sh
+++ osx/setup.sh
@@ -19,12 +19,22 @@ echo 'You are using' $storage #Confirming selection
 echo ' ' 
 
 # Making a safe back-up
-echo ' ' 
+echo 'Saving some back-ups to be on the safe side...'
+mkdir ~/Documents/minecraft.old
+cp -r ~/Library/Application\ Support/minecraft/saves ~/Documents/minecraft.old/saves.old
+cp -r ~/Library/Application\ Support/minecraft/resourcepacks ~/Documents/minecraft.old/resourcepacks.old
+echo 'Backup can be found in ~/Documents/minecraft.old'
+
+echo ' ' # Making it all happen
+echo 'Moving some files around'
+mkdir ~/$storage/minecraft
+mv ~/Library/Application\ Support/minecraft/saves ~/$storage/minecraft/saves
+mv ~/Library/Application\ Support/minecraft/resourcepacks ~/$storage/minecraft/resourcepacks
 
 echo ' ' 
 echo 'Setting up links...'
-# ln -s ~/$storage/minecraft/saves/ ~/Library/Application\ Support/minecraft/saves
-# ln -s ~/Dropbox/minecraft/resourcepacks/ ~/Library/Application\ Support/minecraft/resourcepacks
+ln -s ~/$storage/minecraft/saves/ ~/Library/Application\ Support/minecraft/saves
+ln -s ~/$storage/minecraft/resourcepacks/ ~/Library/Application\ Support/minecraft/resourcepacks
 
 echo ' '
 echo 'Thanks for using my app!'
